f1feb1c62c43baf8086f6efde8eed51180722699
Mario David <mariodavid@arcor.de>
2015-06-29 13:44:55 +0200

    logo usw.

---
+++ modules/web/src/de/csh/patientenverwaltung/web/ExtMainWindow.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2015 de.csh.patientenverwaltung.web
+ */
+package de.csh.patientenverwaltung.web;
+
+import com.haulmont.cuba.web.app.mainwindow.AppMainWindow;
+
+/**
+ * @author mario
+ */
+public class ExtMainWindow extends AppMainWindow {
+}
